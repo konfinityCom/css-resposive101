97cd00d51d820760f15cc71a43d5852147bce06d
kamal <kamal_pandey2012@hotmail.com>
2017-04-13 20:24:43 +0530

    multinavigation on phone

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # css-resposive101
+
 <h2>Building a Responsive start file</h2>
 <ul>
     <li>version 0.0 - project skeleton </li>
@@ -11,9 +12,19 @@
     <li>version 0.3.1 - Tablet enchancement </li>
     <li>version 0.3.2 - Desktop Enhancement </li>
 </ul>
+
+
 <h2>Navigation using JS show hide</h2>
     <li>version 0.4.0 - Connect JS and a button </li>
     <li>version 0.4.1 - Style the phone navigation</li>
     <li>version 0.4.1 - Style the tablet navigation</li>
     <li>version 0.4.2 - Bug fix for menu open phone to tablet transition</li>
 <ul>
+
+
+<h2>Navigation with submenus</h2>
+<ul>
+<li>version 0.5.0 - navigation on phone </li>
+<li> version 0.5.1 - Navigation on Tablet </li>
+<li> version 0.5.2 - Navigation on desktop </li>
+</ul> 
--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,30 @@
 var navButton;
 var navContainer;
 var navClicked = false;
+var navFlavor;
+var subNavContainer;
+var carrot ;
 
 document.addEventListener('DOMContentLoaded', function(){
     
     navButton = document.getElementById('nav-button');
     navContainer = document.getElementById('nav-container');
+    navFlavor = document.getElementById('nav-flavor');
+    subNavContainer = document.getElementById('subnav-container');
+    carrot = document.getElementById('carrot');
+    navFlavorMenu = document.getElementById('')
     navButton.addEventListener('click', navButtonClicked);
+    navFlavor.addEventListener('click', navFlavorClicked);
 });
 
 function navButtonClicked(){
     setNavigationButtonText();
     navContainer.classList.toggle('show');
+}
+
+function navFlavorClicked(){
+    subNavContainer.classList.toggle('show');
+    subNavContainer.classList.contains('show') ? carrot.innerHTML = ' &#9650;':carrot.innerHTML = ' &#9660;'    
 }
 
 function setNavigationButtonText(){
